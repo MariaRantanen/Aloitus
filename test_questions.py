26f4af43b93648275d75b8fa272330a3c6210bf2
MariaRantanen <122267909+MariaRantanen@users.noreply.github.com>
2023-03-14 11:31:24 +0200

    testit staticmethodille

--- test_questions.py
+++ test_questions.py
@@ -34,8 +34,7 @@ def test_ask_user_float(monkeypatch):
     user_input = '1.5'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (
+    assert questions.Question.ask_user_float("Anna liukuluku: ", False) == (
         1.5, 'OK', 0, 'Conversion successful')
 
 # Test an error condition when user adds a unit to a floating point number
@@ -46,8 +45,7 @@ def test_ask_user_float2(monkeypatch):
     user_input = '1.5v'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (
+    assert questions.Question.ask_user_float("Anna kokonaisluku", False) == (
         0, 'Error', 1, "could not convert string to float: '1.5v'")
 
 # Test an error condition when user uses comma instead of dot as decimal separator
@@ -58,8 +56,7 @@ def test_ask_user_float3(monkeypatch):
     user_input = '74,6'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Anna kokonaisluku')
-    assert question.ask_user_float(False) == (
+    assert questions.Question.ask_user_float("Anna kokonaisluku", False) == (
         0, 'Error', 1, "could not convert string to float: '74,6'")
 
 # Test conversion to boolean: case Y
@@ -70,8 +67,7 @@ def test_ask_user_boolean(monkeypatch):
     user_input = 'y'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == (
+    assert questions.Question.ask_user_boolean("Haluatko jatkaa", 'Y', 'N', False) == (
         True, 'OK', 0, 'Conversion successful')
 
 # Test conversion to boolean: case N
@@ -82,8 +78,7 @@ def test_ask_user_boolean2(monkeypatch):
     user_input = 'n'
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
-    question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == (
+    assert questions.Question.ask_user_boolean("Haluatko jatkaa", 'Y', 'N', False) == (
         False, 'OK', 0, 'Conversion successful')
 
 # Test an error condition
@@ -95,5 +90,5 @@ def test_ask_user_boolean3(monkeypatch):
     # Use anonymous function to create input from variable
     monkeypatch.setattr('builtins.input', lambda _: user_input)
     question = questions.Question('Haluatko jatkaa')
-    assert question.ask_user_boolean('Y', 'N', False) == (
+    assert questions.Question.ask_user_boolean("Haluatko jatkaa", 'Y', 'N', False) == (
         'N/A', 'Error', 1, 'unable to convert to boolean')
